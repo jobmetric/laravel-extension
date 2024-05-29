4096659052751feeff0e1c13f6c8c29b89783fda
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-29 23:39:20 +0330

    add service provider

---
+++ src/ExtensionServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JobMetric\Extension;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class ExtensionServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-extension')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation();
+    }
+}
