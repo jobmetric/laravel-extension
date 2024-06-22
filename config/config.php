5658407c2cf8bb47699acf17522dfa6947ca4d61
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-22 22:31:18 +0330

    add config file

--- config/config.php
+++ config/config.php
@@ -17,4 +17,17 @@ return [
 
     "cache_time" => env("EXTENSION_CACHE_TIME", 0),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'extension' => 'extensions',
+        'plugin' => 'plugins',
+    ],
+
 ];
