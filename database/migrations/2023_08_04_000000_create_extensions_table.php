acb7019519837cb071f1df9f5523fae3617fb155
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-22 22:30:51 +0330

    add migration files

---
+++ database/migrations/2023_08_04_000000_create_extensions_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(config('extension.tables.extension'), function (Blueprint $table) {
+            $table->id();
+
+            $table->string('extension')->index();
+            /**
+             * This is the name of each Extension.
+             */
+
+            $table->string('name')->index();
+            /**
+             * This is the name of each Extension.
+             */
+
+            $table->json('info')->nullable();
+            /**
+             * Information of the extension, this field will be filled from the installer file during installation.
+             *
+             * @example
+             * {
+             *    "title": "Extension Name",
+             *    "description": "Description of the extension",
+             *    "version": "1.0.0",
+             *    "author": "Author Name",
+             *    "email": "Author Email",
+             *    "website": "Author Website",
+             *    "creationDate": "2023-08-04",
+             *    "copyright": "Copyright",
+             *    "license": "License",
+             * }
+             */
+
+            $table->timestamps();
+        });
+
+        cache()->forget('extension');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('extension.tables.extension'));
+
+        cache()->forget('extension');
+    }
+};
---
+++ database/migrations/2023_08_04_000001_create_plugins_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(config('extension.tables.plugin'), function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('extension_id')->index()->constrained(config('extension.tables.extension'))->cascadeOnDelete()->cascadeOnUpdate();
+            /**
+             * Extension ID is the ID of the extension.
+             */
+
+            $table->string('title')->index();
+            /**
+             * This is the title of each Extension.
+             */
+
+            $table->json('options')->nullable();
+            /**
+             * Options of the extension, this field will be filled from the installer file during installation.
+             *
+             * @example
+             * {
+             *    "key": "value"
+             * }
+             */
+
+            $table->boolean('status')->default(true)->index();
+            /**
+             * If the layout is not active, it will not be displayed in the layout list.
+             */
+
+            $table->timestamps();
+        });
+
+        cache()->forget('plugin');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('extension.tables.plugin'));
+
+        cache()->forget('plugin');
+    }
+};
