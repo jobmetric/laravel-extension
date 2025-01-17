3a4c17d26e81896c6855943168b293203634ef77
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-18 00:01:45 +0330

    changed extension resource

--- src/Http/Resources/ExtensionResource.php
+++ src/Http/Resources/ExtensionResource.php
@@ -25,15 +25,25 @@ class ExtensionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'extension' => $this->extension,
-            'name' => $this->name,
-            'info' => $this->info,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'plugin_count' => $this->plugin_count,
-
-            'plugins' => $this->whenLoaded('plugins', PluginResource::collection($this->plugins)),
+            'id' => $this['data']['id'] ?? null,
+            'extension' => $this['extension'],
+            'name' => $this['name'],
+            'version' => $this['version'],
+            'title' => trans($this['title']),
+            'description' => trans($this['description']),
+            'author' => $this['author'] ?? '',
+            'email' => $this['email'] ?? '',
+            'website' => $this['website'] ?? '',
+            'creation_at' => $this['creationDate'] ?? '',
+            'copyright' => $this['copyright'] ?? '',
+            'license' => $this['license'] ?? '',
+            'multiple' => $this['multiple'],
+            'namespace' => $this['namespace'],
+            'deletable' => $this['deletable'],
+            'installed' => $this['installed'],
+            'installed_at' => $this['data']['created_at'] ?? '',
+            'updated_at' => $this['data']['updated_at'] ?? '',
+            'plugin_count' => $this['data']['plugin_count'] ?? 0,
         ];
     }
 }
