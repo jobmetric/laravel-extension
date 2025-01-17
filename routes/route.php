ca852f37404bc955d9305af811c72588ee28204c
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-18 00:04:50 +0330

    added route file

---
+++ routes/route.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use JobMetric\Extension\Http\Controllers\ExtensionController;
+use JobMetric\Panelio\Facades\Middleware;
+
+/*
+|--------------------------------------------------------------------------
+| Laravel Extension Routes
+|--------------------------------------------------------------------------
+|
+| All Route in Laravel Extension package
+|
+*/
+
+// Extension
+Route::prefix('p/{panel}/{section}/extension')->name('extension.')->namespace('JobMetric\Extension\Http\Controllers')->group(function () {
+    Route::middleware(Middleware::getMiddlewares())->group(function () {
+        Route::options('{type}', [ExtensionController::class, 'options'])->name('options');
+        Route::resource('{type}', ExtensionController::class)->except(['show', 'destroy'])->parameter('{type}', 'jm_extension:id');
+    });
+});
