5c5259c0af942d7ffadb0ef794b9490840289e9e
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-19 00:36:23 +0330

    added script list file

---
+++ assets/js/list.js
@@ -0,0 +1,207 @@
+// Toggle child row on click
+function listShowDetails(data) {
+    let html = `<div class="row">
+                    <div class="col-12">
+                        <div class="card card-xxl-stretch mb-xl-8 theme-dark-bg-body h-xl-100">
+                            <div class="card-body d-flex flex-column pb-0">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="fs-5 text-dark fw-bold lh-1">${data.description}</div>
+                                    </div>
+                                </div>
+                                <div class="row mt-10">
+                                    <div class="col-12">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.namespace')}</div>
+                                            <div class="fs-5 text-dark fw-bold lh-1">${data.namespace}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 mt-3">
+                        <div class="card card-xxl-stretch mb-xl-8 theme-dark-bg-body h-xl-100">
+                            <div class="card-body d-flex flex-column pb-0">
+                                <div class="row g-0">
+                                    <div class="col-12">
+                                        <div class="d-flex align-items-center mb-9 me-2">
+                                            <div class="symbol symbol-40px me-3">
+                                                <div class="symbol-label bg-light">
+                                                    <i class="ki-duotone ki-calendar fs-1 text-dark">
+                                                        <span class="path1"></span>
+                                                        <span class="path2"></span>
+                                                    </i>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.creation_at')}</div>
+                                                <div class="fs-5 text-dark fw-bold lh-1" dir="ltr">${data.creation_at ?? ''}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-flex align-items-center mb-9 me-2">
+                                            <div class="symbol symbol-40px me-3">
+                                                <div class="symbol-label bg-light">
+                                                    <i class="ki-duotone ki-calendar fs-1 text-dark">
+                                                        <span class="path1"></span>
+                                                        <span class="path2"></span>
+                                                    </i>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.installed_at')}</div>
+                                                <div class="fs-5 text-dark fw-bold lh-1" dir="ltr">${data.installed_at}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-flex align-items-center me-2">
+                                            <div class="symbol symbol-40px me-3">
+                                                <div class="symbol-label bg-light">
+                                                    <i class="ki-duotone ki-calendar fs-1 text-dark">
+                                                        <span class="path1"></span>
+                                                        <span class="path2"></span>
+                                                    </i>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.updated_at')}</div>
+                                                <div class="fs-5 text-dark fw-bold lh-1" dir="ltr">${data.updated_at}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8 mt-3">
+                        <div class="card card-xxl-stretch mb-xl-8 theme-dark-bg-body h-xl-100">
+                            <div class="card-body d-flex flex-column pb-0">
+                                <div class="row g-0">
+                                    <div class="col-12">
+                                        <div class="d-flex justify-content-between align-items-center mb-9 me-2">
+                                            <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.website')}</div>
+                                            <div class="fs-5 text-dark fw-bold lh-1">${data.website ?? ''}</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-flex justify-content-between align-items-center mb-9 me-2">
+                                            <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.email')}</div>
+                                            <div class="fs-5 text-dark fw-bold lh-1">${data.email ?? ''}</div>
+                                        </div>
+                                    </div>`
+                                    if (data.deletable) {
+                                        html += `
+                                        <div class="col-12">
+                                            <div class="d-flex justify-content-between align-items-center me-2">
+                                                <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.delete_note')}</div>
+                                                <a href="${data.id}/delete" class="btn btn-sm btn-outline btn-outline-dashed bg-light-danger btn-color-gray-800">
+                                                    <i class="la la-trash fs-2 position-absolute"></i>
+                                                    <span class="ps-9">${getLocalize('extension.language.delete')}</span>
+                                                </a>
+                                            </div>
+                                        </div>`
+                                    }
+                           html += `
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 mt-3">
+                        <div class="card card-xxl-stretch theme-dark-bg-body h-xl-100">
+                            <div class="card-body d-flex flex-column pb-10">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="fs-5 text-dark fw-bold lh-1" dir="ltr">${data.copyright}</div>
+                                    </div>
+                                </div>
+                                <div class="row mt-10">
+                                    <div class="col-12">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="fs-7 text-gray-600 fw-bold">${getLocalize('extension.language.license')}</div>
+                                            <div class="fs-5 text-dark fw-bold lh-1">${data.license}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+
+    return html
+}
+
+loadScriptsSequentially([
+    'assets/vendor/package-core/js/datatable-columns.js',
+], function() {
+    $(document).ready(function(){
+        dt = $('#datatable').DataTable({
+            responsive: false,
+            autoWidth: false,
+            data: getLocalize('extension.extensions'),
+            columns: [
+                // show details
+                {
+                    data: datatableColumnShowDetailsList,
+                    sortable: false
+                },
+                // title
+                {
+                    name: 'title',
+                    data: function(e) {
+                        return `<div class="align-start text-gray-800 word-no-break">${e.title}</div>`
+                    },
+                    sortable: true
+                },
+                // version
+                {
+                    name: 'version',
+                    data: function(e) {
+                        return `<div class="align-center text-gray-800 word-no-break">${e.version}</div>`
+                    },
+                    sortable: true
+                },
+                // author
+                {
+                    name: 'author',
+                    data: function(e) {
+                        return `<div class="align-center text-gray-800 word-no-break">${e.author}</div>`
+                    },
+                    sortable: true
+                },
+                // action
+                {
+                    data: function(e) {
+                        if (e.installed) {
+                            return `<div class="d-flex justify-content-center align-items-center">
+                                        <a href="${e.id}/uninstall" class="btn btn-sm btn-outline btn-outline-dashed bg-light-danger btn-color-gray-800">
+                                            <i class="la la-times fs-2 position-absolute"></i>
+                                            <span class="ps-9">${getLocalize('extension.language.buttons.uninstall')}</span>
+                                        </a>
+                                    </div>`
+                        } else {
+                            return `<div class="d-flex justify-content-center align-items-center">
+                                        <a href="${e.id}/install" class="btn btn-sm btn-outline btn-outline-dashed bg-light-success btn-color-gray-800">
+                                            <i class="la la-download fs-2 position-absolute"></i>
+                                            <span class="ps-9">${getLocalize('extension.language.buttons.install')}</span>
+                                        </a>
+                                    </div>`
+                        }
+                    },
+                    sortable: false
+                }
+            ],
+            order: [
+                [1, "asc"]
+            ],
+            searching: false,
+            lengthChange: false,
+            deferRender: true,
+            pageLength: localize.list_view.page_limit,
+            language: localize.language.datatable
+        })
+    })
+})
