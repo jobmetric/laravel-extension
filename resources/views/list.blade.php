774f47ef8443ddef7091dcee2fdfcf3a00501538
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-19 00:36:14 +0330

    added blade list file

---
+++ resources/views/list.blade.php
@@ -0,0 +1,21 @@
+@extends('panelio::layout.layout')
+
+@section('body')
+    <x-list-view name="{{ $label }}" action="{{ $route }}">
+        <thead>
+            <tr>
+                <th width="1%"></th>
+                <th width="42%" class="text-gray-800 auto-width-content">{{ trans('extension::base.list.columns.name') }}</th>
+                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.version') }}</th>
+                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.author') }}</th>
+                <th width="15%" class="text-center text-gray-800">{{ trans('package-core::base.list.columns.action') }}</th>
+            </tr>
+        </thead>
+    </x-list-view>
+
+    @if($hasShowDescriptionInList)
+        <div class="mt-10">
+            <h6>{{ $description ?? '' }}</h6>
+        </div>
+    @endif
+@endsection
