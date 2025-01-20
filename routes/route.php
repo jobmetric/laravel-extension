76060908592b28801b07edf356c53359b5420587
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-21 00:02:09 +0330

    added install route

--- routes/route.php
+++ routes/route.php
@@ -16,6 +16,7 @@ use JobMetric\Panelio\Facades\Middleware;
 // Extension
 Route::prefix('p/{panel}/{section}/extension')->name('extension.')->namespace('JobMetric\Extension\Http\Controllers')->group(function () {
     Route::middleware(Middleware::getMiddlewares())->group(function () {
+        Route::post('{type}/install', [ExtensionController::class, 'install'])->name('install');
         Route::options('{type}', [ExtensionController::class, 'options'])->name('options');
         Route::resource('{type}', ExtensionController::class)->except(['show', 'destroy'])->parameter('{type}', 'jm_extension:id');
     });
