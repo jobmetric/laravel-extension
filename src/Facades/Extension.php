eeea6fd96edc5544778c54b821e6d2cc5daeca74
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-23 00:23:16 +0330

    changed uninstall method in facade

--- src/Facades/Extension.php
+++ src/Facades/Extension.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static array all(string $extension, array $filter = [], array $with = [])
  * @method static \JobMetric\Extension\Models\Extension|\JobMetric\Extension\Http\Resources\ExtensionResource getInfo(string $extension, string $name, bool $has_resource = false)
  * @method static array install(string $namespace)
- * @method static array uninstall(string $extension, string $name, bool $force_delete_plugin = false)
+ * @method static array uninstall(string $namespace, bool $force_delete_plugin = false)
  * @method static array update(string $extension, string $name)
  * @method static void installZip(string $path, bool $delete_file = false)
  * @method static void download(string $path)
