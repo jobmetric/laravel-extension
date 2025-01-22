9d6eba4c6c0bd65cec1ad92e37b3961545850369
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-23 00:23:31 +0330

    changed some exception files

--- src/Exceptions/ExtensionNotInstalledException.php
+++ src/Exceptions/ExtensionNotInstalledException.php
@@ -7,10 +7,9 @@ use Throwable;
 
 class ExtensionNotInstalledException extends Exception
 {
-    public function __construct(string $extension, string $name, int $code = 400, ?Throwable $previous = null)
+    public function __construct(string $name, int $code = 400, ?Throwable $previous = null)
     {
         parent::__construct(trans('extension::base.exceptions.extension_not_installed', [
-            'extension' => $extension,
             'name' => $name
         ]), $code, $previous);
     }
