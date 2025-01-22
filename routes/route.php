2beca9d48d340902130546ea88ae26b328d0c130
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-23 00:26:08 +0330

    added uninstall route

--- routes/route.php
+++ routes/route.php
@@ -17,6 +17,7 @@ use JobMetric\Panelio\Facades\Middleware;
 Route::prefix('p/{panel}/{section}/extension')->name('extension.')->namespace('JobMetric\Extension\Http\Controllers')->group(function () {
     Route::middleware(Middleware::getMiddlewares())->group(function () {
         Route::post('{type}/install', [ExtensionController::class, 'install'])->name('install');
+        Route::post('{type}/uninstall', [ExtensionController::class, 'uninstall'])->name('uninstall');
         Route::options('{type}', [ExtensionController::class, 'options'])->name('options');
         Route::resource('{type}', ExtensionController::class)->except(['show', 'destroy'])->parameter('{type}', 'jm_extension:id');
     });
