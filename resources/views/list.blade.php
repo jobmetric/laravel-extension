8a3aa1a7f67913d985517bebf349e413c6a8d3e0
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-24 00:51:46 +0330

    changed blade extension list file

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,10 +5,10 @@
         <thead>
             <tr>
                 <th width="1%"></th>
-                <th width="42%" class="text-gray-800 auto-width-content">{{ trans('extension::base.list.columns.name') }}</th>
+                <th width="39%" class="text-gray-800 auto-width-content">{{ trans('extension::base.list.columns.name') }}</th>
                 <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.version') }}</th>
                 <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.author') }}</th>
-                <th width="15%" class="text-center text-gray-800">{{ trans('package-core::base.list.columns.action') }}</th>
+                <th width="40%" class="text-center text-gray-800">{{ trans('package-core::base.list.columns.action') }}</th>
             </tr>
         </thead>
     </x-list-view>
