456178872a9401d43ead30979567424421a384c6
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-24 18:42:49 +0330

    changed language in blade file

--- resources/views/extension/list.blade.php
+++ resources/views/extension/list.blade.php
@@ -5,9 +5,9 @@
         <thead>
             <tr>
                 <th width="1%"></th>
-                <th width="39%" class="text-gray-800 auto-width-content">{{ trans('extension::base.list.columns.name') }}</th>
-                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.version') }}</th>
-                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.columns.author') }}</th>
+                <th width="39%" class="text-gray-800 auto-width-content">{{ trans('extension::base.list.extension.columns.name') }}</th>
+                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.extension.columns.version') }}</th>
+                <th width="10%" class="text-center text-gray-800">{{ trans('extension::base.list.extension.columns.author') }}</th>
                 <th width="40%" class="text-center text-gray-800">{{ trans('package-core::base.list.columns.action') }}</th>
             </tr>
         </thead>
