0961949ebff8259d8255d64124f64510668378bc
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-20 20:01:03 +0330

    register default type, use from config file

--- config/config.php
+++ config/config.php
@@ -15,4 +15,61 @@ return [
         'plugin' => 'plugins',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Extension Types (Registry defaults)
+    |--------------------------------------------------------------------------
+    |
+    | Types registered by default in ExtensionTypeRegistry. You can register
+    | more at runtime via ExtensionTypeRegistry::register() or in a service
+    | provider. Each key is the type name, value is optional options array.
+    */
+
+    'types' => [
+        'Module' => [
+            'label' => 'extension::base.types.module.label',
+            'description' => 'extension::base.types.module.description',
+        ],
+        'ShippingMethod' => [
+            'label' => 'extension::base.types.shipping_method.label',
+            'description' => 'extension::base.types.shipping_method.description',
+        ],
+        'PaymentMethod' => [
+            'label' => 'extension::base.types.payment_method.label',
+            'description' => 'extension::base.types.payment_method.description',
+        ],
+        'OnlinePaymentMethod' => [
+            'label' => 'extension::base.types.online_payment_method.label',
+            'description' => 'extension::base.types.online_payment_method.description',
+        ],
+        'OrderTotal' => [
+            'label' => 'extension::base.types.order_total.label',
+            'description' => 'extension::base.types.order_total.description',
+        ],
+        'ReturnOrderTotal' => [
+            'label' => 'extension::base.types.return_order_total.label',
+            'description' => 'extension::base.types.return_order_total.description',
+        ],
+        'Captcha' => [
+            'label' => 'extension::base.types.captcha.label',
+            'description' => 'extension::base.types.captcha.description',
+        ],
+        'Report' => [
+            'label' => 'extension::base.types.report.label',
+            'description' => 'extension::base.types.report.description',
+        ],
+        'Theme' => [
+            'label' => 'extension::base.types.theme.label',
+            'description' => 'extension::base.types.theme.description',
+        ],
+        'AttendanceDevice' => [
+            'label' => 'extension::base.types.attendance_device.label',
+            'description' => 'extension::base.types.attendance_device.description',
+        ],
+        'SyncingSoftware' => [
+            'label' => 'extension::base.types.syncing_software.label',
+            'description' => 'extension::base.types.syncing_software.description',
+        ],
+    ],
+
 ];
--- src/ExtensionServiceProvider.php
+++ src/ExtensionServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace JobMetric\Extension;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Route;
 use JobMetric\Extension\Models\Extension as ExtensionModel;
 use JobMetric\Extension\Models\Plugin as PluginModel;
+use JobMetric\Extension\Support\ExtensionTypeRegistry;
 use JobMetric\PackageCore\Enums\RegisterClassTypeEnum;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
@@ -31,18 +33,26 @@ class ExtensionServiceProvider extends PackageCoreServiceProvider
             ->registerCommand(Commands\ExtensionUninstallCommand::class)
             ->registerClass('Plugin', Plugin::class)
             ->registerClass('Extension', Extension::class)
-            ->registerClass('ExtensionType', ExtensionType::class, RegisterClassTypeEnum::SINGLETON());
+            ->registerClass('ExtensionType', ExtensionType::class, RegisterClassTypeEnum::SINGLETON())
+            ->registerClass('ExtensionTypeRegistry', ExtensionTypeRegistry::class, RegisterClassTypeEnum::SINGLETON());
     }
 
     /**
      * After register package
      *
      * @return void
+     * @throws BindingResolutionException
      */
     public function afterRegisterPackage(): void
     {
-        // Register model binding
+        // register route model bindings
         Route::model('jm_extension', ExtensionModel::class);
         Route::model('jm_plugin', PluginModel::class);
+
+        // register extension types
+        $registry = $this->app->make('ExtensionTypeRegistry');
+        foreach (config('extension.types', []) as $type => $options) {
+            $registry->register($type, is_array($options) ? $options : []);
+        }
     }
 }
