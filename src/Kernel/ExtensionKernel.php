f83bb038368cacb29495e12810426cd960967c86
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-23 15:30:30 +0330

    added trait ExtensionKernelCallbacks to ExtensionKernel

--- src/Kernel/ExtensionKernel.php
+++ src/Kernel/ExtensionKernel.php
@@ -45,82 +45,14 @@ use JobMetric\Extension\Models\Extension as ExtensionModel;
  */
 class ExtensionKernel
 {
+    use ExtensionKernelCallbacks;
+
     /**
      * Extension instances loaded from the extensions table (installed only).
      *
      * @var array<int, AbstractExtension>
      */
     protected array $extensions = [];
-
-    /**
-     * Callbacks run before register() is invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $registeringCallbacks = [];
-
-    /**
-     * Callbacks run after register() has been invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $registeredCallbacks = [];
-
-    /**
-     * Callbacks run before boot() is invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $bootingCallbacks = [];
-
-    /**
-     * Callbacks run after boot() has been invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $bootedCallbacks = [];
-
-    /**
-     * Callbacks run before activate() is invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $activatingCallbacks = [];
-
-    /**
-     * Callbacks run after activate() has been invoked on all extensions.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $activatedCallbacks = [];
-
-    /**
-     * Callbacks run before discover() scans the filesystem.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $discoveringCallbacks = [];
-
-    /**
-     * Callbacks run after discover() has registered FQCNs in ExtensionRegistry.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $discoveredCallbacks = [];
-
-    /**
-     * Callbacks run before loadInstalledExtensions() queries the database.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $loadingInstalledCallbacks = [];
-
-    /**
-     * Callbacks run after loadInstalledExtensions() has added instances to the kernel.
-     *
-     * @var array<int, callable(self): void>
-     */
-    protected array $loadedInstalledCallbacks = [];
 
     /**
      * @param Application $app
@@ -138,146 +70,6 @@ class ExtensionKernel
     public function app(): Application
     {
         return $this->app;
-    }
-
-    /**
-     * Register a callback to run before extension register() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel (e.g. to use app() or extensions()).
-     *
-     * @return self
-     */
-    public function registering(callable $callback): self
-    {
-        $this->registeringCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run after extension register() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel.
-     *
-     * @return self
-     */
-    public function registered(callable $callback): self
-    {
-        $this->registeredCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run before extension boot() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel.
-     *
-     * @return self
-     */
-    public function booting(callable $callback): self
-    {
-        $this->bootingCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run after extension boot() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel.
-     *
-     * @return self
-     */
-    public function booted(callable $callback): self
-    {
-        $this->bootedCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run before extension activate() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel.
-     *
-     * @return self
-     */
-    public function activating(callable $callback): self
-    {
-        $this->activatingCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run after extension activate() phase.
-     *
-     * @param callable(ExtensionKernel): void $callback Receives this kernel.
-     *
-     * @return self
-     */
-    public function activated(callable $callback): self
-    {
-        $this->activatedCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run before discover() scans the filesystem.
-     *
-     * @param callable(ExtensionKernel): void $callback
-     *
-     * @return self
-     */
-    public function discovering(callable $callback): self
-    {
-        $this->discoveringCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run after discover() has registered FQCNs in ExtensionRegistry.
-     *
-     * @param callable(ExtensionKernel): void $callback
-     *
-     * @return self
-     */
-    public function discovered(callable $callback): self
-    {
-        $this->discoveredCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run before loadInstalledExtensions() queries the database.
-     *
-     * @param callable(ExtensionKernel): void $callback
-     *
-     * @return self
-     */
-    public function loadingInstalled(callable $callback): self
-    {
-        $this->loadingInstalledCallbacks[] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Register a callback to run after loadInstalledExtensions() has added instances to the kernel.
-     *
-     * @param callable(ExtensionKernel): void $callback
-     *
-     * @return self
-     */
-    public function loadedInstalled(callable $callback): self
-    {
-        $this->loadedInstalledCallbacks[] = $callback;
-
-        return $this;
     }
 
     /**
@@ -657,16 +449,7 @@ class ExtensionKernel
     public function reset(): self
     {
         $this->extensions = [];
-        $this->registeringCallbacks = [];
-        $this->registeredCallbacks = [];
-        $this->bootingCallbacks = [];
-        $this->bootedCallbacks = [];
-        $this->activatingCallbacks = [];
-        $this->activatedCallbacks = [];
-        $this->discoveringCallbacks = [];
-        $this->discoveredCallbacks = [];
-        $this->loadingInstalledCallbacks = [];
-        $this->loadedInstalledCallbacks = [];
+        $this->clearCallbacks();
 
         return $this;
     }
